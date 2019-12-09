bca0c3922a1532b7ea932fa5f9c39d01958117b3
Jinwoo Jeong <thinkratomos@gmail.com>
2019-12-10 00:28:47 +0900

    Add interface for type error.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,11 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export default class KAIRENDocument extends Document {
+interface DocumentProps {
+  styleTags: any
+}
+
+export default class KAIRENDocument extends Document<DocumentProps> {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet()
     const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />))
